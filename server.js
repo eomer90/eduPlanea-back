97844d56c7ee05dd6c4ffae61a873d7182f1b402
Efren Gutierrez Torres <efrengutierreztorres@gmail.com>
2026-09-05 16:37:09 -0600

    patch evaluaciones

--- server.js
+++ server.js
@@ -187,6 +187,54 @@ server.get(ALUMNOS_ROUTE, verificarToken, async (req, res) => {
   }
 });
 
+server.patch("/alumnos/evaluacion", verificarToken, async (req, res) => {
+  try {
+    const { materia, evaluacion } = req.body;
+
+    for (const resultado of evaluacion.resultados) {
+      const alumno = await Alumnos.findOne({
+        _id: resultado.alumnoId,
+        usuarioId: req.usuarioId,
+        escuelaId: req.escuelaId,
+      });
+
+      if (!alumno) {
+        continue;
+      }
+
+      const materiaAlumno = alumno.materias.find((m) => m.nombre === materia);
+
+      if (!materiaAlumno) {
+        continue;
+      }
+
+      materiaAlumno.evaluaciones.push({
+        nombre: evaluacion.nombre,
+        fecha: evaluacion.fecha,
+        instrumento: evaluacion.instrumento,
+        cuantitativa: evaluacion.cuantitativa,
+        cualitativa: evaluacion.cualitativa,
+        contenidoId: evaluacion.contenidoId,
+        pdaIds: evaluacion.pdaIds,
+        resultados: [resultado],
+      });
+
+      await alumno.save();
+    }
+
+    res.status(200).json({
+      mensaje: "Evaluación guardada correctamente",
+    });
+  } catch (error) {
+    console.log("ERROR EVALUACION:", error);
+
+    res.status(500).json({
+      error: true,
+      mensaje: "Error al guardar la evaluación",
+    });
+  }
+});
+
 server.get(`${ALUMNOS_ROUTE}/:id`, verificarToken, async (req, res) => {
   try {
     const id = req.params.id;
@@ -465,55 +513,5 @@ server.get(ESCUELAS_ROUTE, async (req, res) => {
   } catch (error) {
     const mensaje = "Error al encontrar escuelas";
     res.status(500).json({ mensaje, error });
-  }
-});
-
-//evaluacion
-
-server.patch("/alumnos/evaluacion", verificarToken, async (req, res) => {
-  try {
-    const { materia, evaluacion } = req.body;
-
-    for (const resultado of evaluacion.resultados) {
-      const alumno = await Alumnos.findOne({
-        _id: resultado.alumnoId,
-        usuarioId: req.usuarioId,
-        escuelaId: req.escuelaId,
-      });
-
-      if (!alumno) {
-        continue;
-      }
-
-      const materiaAlumno = alumno.materias.find((m) => m.nombre === materia);
-
-      if (!materiaAlumno) {
-        continue;
-      }
-
-      materiaAlumno.evaluaciones.push({
-        nombre: evaluacion.nombre,
-        fecha: evaluacion.fecha,
-        instrumento: evaluacion.instrumento,
-        cuantitativa: evaluacion.cuantitativa,
-        cualitativa: evaluacion.cualitativa,
-        contenidoId: evaluacion.contenidoId,
-        pdaIds: evaluacion.pdaIds,
-        resultados: [resultado],
-      });
-
-      await alumno.save();
-    }
-
-    res.status(200).json({
-      mensaje: "Evaluación guardada correctamente",
-    });
-  } catch (error) {
-    console.log(error);
-
-    res.status(500).json({
-      error: true,
-      mensaje: "Error al guardar la evaluación",
-    });
   }
 });
